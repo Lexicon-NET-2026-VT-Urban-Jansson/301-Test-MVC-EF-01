a1eaa7f20bcdc1859391c80963ec8b9c47e06026
Urban Jansson <dev.urban.jansson@outlook.com>
2026-08-02 14:57:03 +0200

    Initialize - Base commit

---
+++ Test-MVC-EF-01/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test_MVC_EF_01.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
---
+++ Test-MVC-EF-01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_MVC_EF_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
